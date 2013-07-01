7ac6814017af76eefa0da3afc6326ac45c6f847b
Shany <shany.lev@thescore.com>
2013-07-01 08:57:37 -0400

    Add Delete Todo functionality

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -19,5 +19,11 @@ Todos.TodoController = Ember.ObjectController.extend({
 	acceptChanges: function() {
 		this.set('isEditing', false);
 		this.get('model').save();
+	},
+
+	removeTodo: function() {
+		var todo = this.get('model');
+		todo.deleteRecord();
+		todo.save();
 	}
 });
